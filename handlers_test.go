8a27b18e09ba411ad2b7c157be44c09f23a56d4f
Gabriel de Laboulaye <gabriel.delaboulaye@schibsted.com>
2018-12-17 13:36:11 +0100

    Add some tests with real data to the handlers tests

--- handlers_test.go
+++ handlers_test.go
@@ -48,3 +48,66 @@ func Test_PopularHandler_returns_200(t *testing.T) {
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 	assert.Equal(t, "null\n", string(body))
 }
+
+func Test_PopularHandler_returns_400_on_empty_date(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "http://localhost/1/popular/", nil)
+	logs := logs{}
+	handler := http.HandlerFunc(GetHandler(logs).Popular)
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	assert.Equal(t, "", string(body))
+}
+
+var exampleLogs = logs{
+	2018: {
+		11: {
+			12: {
+				17: {
+					52: {
+						59: {
+							"google.com": 3,
+						},
+					},
+				},
+				19: {
+					11: {
+						13: {
+							"google.com": 7,
+						},
+						14: {
+							"other": 1,
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
+func Test_CountHandler_returns_200_with_counter(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "http://localhost/1/count/2018", nil)
+	handler := http.HandlerFunc(GetHandler(exampleLogs).Count)
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.JSONEq(t, `{"count":2}`, string(body))
+}
+
+func Test_PopularHandler_returns_200_with_results_limited(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "http://localhost/1/popular/2018?limit=1", nil)
+	handler := http.HandlerFunc(GetHandler(exampleLogs).Popular)
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.JSONEq(t, `[{"counter":10, "query": "google.com"}]`, string(body))
+}
